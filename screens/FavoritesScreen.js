662abebd2d90bf7c7d8b21dc6b82207e3b13c5a2
Adnan Quraishi <adnanquraishi00@gmail.com>
2024-02-21 00:17:51 +0500

    Initial_commit
    Initial Commit (Meals app State Management Context Api)

---
+++ screens/FavoritesScreen.js
@@ -0,0 +1,40 @@
+
+import { View,Text,Image,StyleSheet } from "react-native";
+import MealsList from "../components/MealsList/MealsList";
+import { useContext } from "react";
+import { MEALS } from "../data/dummy-data";
+import  {FavoritesContext}  from '../store/context/favorites-context';
+
+
+function FavoritesScreen(){
+    const favoriteMealContext = useContext(FavoritesContext);
+    
+    const favoriteMeals = MEALS.filter(meals => favoriteMealContext.ids.includes(meals.id));
+    console.log(favoriteMeals);
+
+
+    if(favoriteMeals.length === 0){
+        return<View style={styles.container}>
+            <Text style={styles.text}>You have no Favorite meals yet...</Text>
+        </View>
+    }
+
+    return(
+        <MealsList items={favoriteMeals}/>
+    );
+}
+export default FavoritesScreen;
+
+const styles = StyleSheet.create({
+    container: {
+
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    text:{
+        fontSize:18,
+        fontWeight:'bold',
+        color:'white'
+    }
+  });
---
+++ screens/MealDetailScreen.js
@@ -0,0 +1,92 @@
+import { View,Text,StyleSheet,Image,ScrollView, Button } from "react-native";
+import { useContext, useLayoutEffect } from "react";
+import {MEALS} from '../data/dummy-data'
+import  {FavoritesContext}  from '../store/context/favorites-context';
+import MealDetails from "../components/MealDetails";
+import Subtitle from "../components/MealDetail/Subtitle";
+import List from "../components/MealDetail/List";
+import IconButton from "../components/IconButton";
+
+
+function MealDetailScreen({route , navigation}){
+
+    const favoriteMealsContext = useContext(FavoritesContext);
+    const mealId = route.params.mealId;
+    const selectedMeal = MEALS.find((meal)=> meal.id === mealId);
+    const mealIsFavorite = favoriteMealsContext.ids.includes(mealId);
+
+    function changeFavoriteStatusHandler(){
+        if(mealIsFavorite){
+            favoriteMealsContext.removeFavorite(mealId);
+        }else{
+            favoriteMealsContext.addFavorite(mealId);
+        }
+    }
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerRight: () => {
+                return <IconButton             
+                icon={mealIsFavorite ? 'star' : 'star-outline'}
+                color="white"
+                onPress={changeFavoriteStatusHandler} />
+            }
+        });
+    },[navigation,changeFavoriteStatusHandler]);
+
+
+    return(
+        <ScrollView style={styles.rootContainer}> 
+           <Image style={styles.imageStyle} source = {{ uri: selectedMeal.imageUrl }}/>
+           <Text  style={styles.title} >{selectedMeal.title}</Text>         
+                <MealDetails 
+                duration={selectedMeal.duration}
+                complexity={selectedMeal.complexity}
+                affordability={selectedMeal.affordability}
+                textStyle={styles.detailText}
+                />
+
+             <View style={styles.listOuterContainer}>
+                <View style={styles.listContainer} >
+                    <Subtitle>Ingredients</Subtitle>
+                    <List data = {selectedMeal.ingredients}/>
+                    <Subtitle>Steps</Subtitle>
+                    <List data = {selectedMeal.steps}/>
+                </View>
+              </View>
+          </ScrollView>
+    );
+}
+export default MealDetailScreen;
+
+const styles = StyleSheet.create({
+    rootContainer: {
+        marginBottom:32
+    },
+
+    imageStyle:{
+        width:'100%',
+        height:350,
+
+    },
+    title:{
+        fontWeight:'bold',
+        fontSize:24,
+        margin:8,
+        textAlign:'center',
+        color:'white'
+
+    },
+    detailText:{
+        color:'white',
+    },
+    listContainer:{
+        maxWidth:'80%',
+
+    },
+    listOuterContainer:{
+        alignItems:'center',
+    }
+
+   
+  });
